8f299a8f504a920392efcc62ef27ab272ba73040
kisan010 <kisandash199@gmail.com>
2025-02-03 17:06:43 +0530

    updating files

--- Components/Contacts.jsx
+++ Components/Contacts.jsx
@@ -18,27 +18,27 @@ export const Contact = () => {
           <div  className="w-1/2">
           <form className="w-full max-w-lg">
   <div className="flex flex-wrap -mx-3 mb-6 flex-col sm:flex-row">
-    <div className="w-50 md:w-1/2 px-3 mb-6 md:mb-0">
+    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
         First Name
       </label>
       <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200  rounded p-3  mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Enter First Name" />
      
     </div>
-    <div className=" w-50 md:w-1/2 px-3" >
+    <div className=" w-full md:w-1/2 px-3" >
       <label style={{'margin-left':'1rem'}}className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
         Last Name
       </label>
       <input style={{'margin-left':'1rem'}} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200  rounded p-3  mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-last-name" type="text" placeholder="Enter last Name"/>
     </div>
-    <div className="w-50 md:w-1/2 px-3 mb-6 md:mb-0 mt ">
+    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 mt ">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-Email-name">
         Email
       </label>
       <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200  rounded p-3 leading-tight focus:outline-none focus:bg-white" id="grid-Email-name" type="text" placeholder="Enter Email" />
      
     </div>
-    <div className="w-50 md:w-1/2 px-3 mt">
+    <div className="w-full md:w-1/2 px-3 mt">
       <label style={{'margin-left':'1rem'}} className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-Subject-name">
        subject
       </label>
